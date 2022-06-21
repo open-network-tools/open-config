bdb61595a5ad425efedd0cd34d828119473480a2
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2022-06-21 19:09:03 +0200

    Update OpenManufacturer.php
    #patch

--- src/OpenManufacturer.php
+++ src/OpenManufacturer.php
@@ -8,6 +8,18 @@
         private $configFile;
         private $configReport = [];
 
+        static $sourceModel = [
+            'ERS 45xx'  => 'extremenetworks-ers-45xx',
+            'ERS 48xx'  => 'extremenetworks-ers-48xx',
+            'ERS 49xx'  => 'extremenetworks-ers-49xx',
+        ];
+        static $destinationModel = [
+            'XOS 5320'  => 'extremenetwork-xos-5320',
+            'XOS 5420'  => 'extremenetwork-xos-5420',
+            'XOS 5520'  => 'extremenetwork-xos-5520',
+            'XOS 5720'  => 'extremenetwork-xos-5720',
+        ];
+
         public function __construct(){
             $this->config = new OpenConfig();
         }
@@ -64,6 +76,14 @@
                 if(array_key_exists($k, $this->configReport)) $check = "\e[32m✓";
                 echo " ".$check."\e[96m  | \e[39m".$v;
             }
+        }
+
+        static function modelManufacturer($value = ""){
+            if(str_contains($value, "extremenetworks") && str_contains($value, "-xos")) return "Extreme Networks - XOS";
+            if(str_contains($value, "extremenetworks") && str_contains($value, "-voss")) return "Extreme Networks - VOSS";
+            if(str_contains($value, "extremenetworks")) return "Extreme Networks";
+            if(str_contains($value, "juniper")) return "Juniper";
+            return "Unknown";
         }
 
     }
